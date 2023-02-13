26f8e2626d1fcba8c792d8a061e3877d4c524ab9
Marat Minchik <devminrat@gmail.com>
2023-02-13 19:58:32 +0300

    added main page

---
+++ src/assets/js/components/sliders.js
@@ -0,0 +1,29 @@
+// const introSlider = new Swiper(".intro-silder", {
+// slidesPerView: "auto",
+// spaceBetween: 37,
+
+// pagination: {
+//   el: ".swiper-pagination",
+//   type: "progressbar",
+// },
+
+// navigation: {
+//   nextEl: ".intro-slider__btn-next",
+//   prevEl: ".intro-slider__btn-prev",
+// },
+
+// breakpoints: {
+//   900: {
+//     spaceBetween: 50,
+//   },
+//   1350: {
+//     slidesPerView: "auto",
+//     spaceBetween: 80,
+//   },
+// },
+//   });
+
+const introSlider = new Swiper(".intro-silder", {
+  slidesPerView: "auto",
+  spaceBetween: 0,
+});
